677174d861f1a94b087df6815ea4ab32f3bb1bd2
panktiparekh15 <ppankti81@gmail.com>
2023-03-27 22:28:46 -0400

    added csv

--- scripts/update.py
+++ scripts/update.py
@@ -7,19 +7,20 @@ def modify(row):
     js["project_category"] = row[1]
     js["project_url"] = row[2]
     js["project_date"] = str(row[3])
-    js["thumbnail_img"] = row[4]
-    js["banner_img"] = row[5]
-    js["tech_stack"] = row[6]
-    js["project_description"] = row[7].split("\n")
+    js["project_img"] = row[4]
+    # js["banner_img"] = row[5]
+    js["tech_stack"] = row[5]
+    js["project_description"] = row[6].split("\n")
     return js
 
 
-data = pd.read_excel("projects.xlsx")
+data = pd.read_csv("projects.csv")
+print(data["project_category"])
 data["project_category"] = data["project_category"].fillna("")
 data["project_url"] = data["project_url"].fillna("")
 data["project_date"] = data["project_date"].fillna("")
-data["thumbnail_img"] = data["thumbnail_img"].fillna("")
-data["banner_img"] = data["banner_img"].fillna("")
+data["project_img"] = data["project_img"].fillna("")
+# data["banner_img"] = data["banner_img"].fillna("")
 data["tech_stack"] = data["tech_stack"].fillna("")
 data["project_description"] = data["project_description"].fillna("")
 data = data.values.tolist()
@@ -29,7 +30,7 @@ for r in data:
     proj.append(modify(r))
    
 result = proj
-
+print(result)
 with open("projects.json", "w") as f:
     string = json.dumps(result)
     f.writelines(string)
@@ -45,7 +46,7 @@ for i in range(0, len(result)):
     file = file.replace("TITLE_HERE", project["project_name"])
     file = file.replace("CATEGORY_HERE", project["project_category"])
     file = file.replace("TECHSTACK_HERE", project["tech_stack"])
-    file = file.replace("IMAGE_HERE", project["banner_img"])
+    file = file.replace("IMAGE_HERE", project["project_img"])
     file = file.replace("PROJECT_DESCRIPTION_HERE", str.join('\n', project["project_description"]))
     file = file.replace("EXCELSHEET_BEFORE", f'{result[(i - 1) % len(result)]["project_url"]}')
     file = file.replace("EXCELSHEET_NEXT", f'{result[(i + 1) % len(result)]["project_url"]}')
